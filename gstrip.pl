ceb7a2751449f10c08d7d87832daeeb691375706
justindstein <jaydstein@gmail.com>
2023-12-31 10:25:32 -0800

    Removal of labels and a lot of unnecesarry logic.

--- gstrip.pl
+++ gstrip.pl
@@ -7,100 +7,62 @@ my $staged = "\e[32m";
 my $unstaged = "\e[31m";
 my $default = "\e[0m";
 
-my $new_regex = qr/^\t(?:new\ file:\s*)(.*)$/;
-my $modified_regex = qr/^\t(?:deleted:\s*)(.*)$/;
-my $deleted_regex = qr/^\t(?:modified:\s*)(.*)$/;
-my $untracked_regex = qr/^\t(?:)(.*)$/;
+# Regexps
+my $filename_regex = qr/^\t(?:new\ file:\s*|deleted:\s*|modified:\s*|)(.*)$/;
+my $staged_regex = qr/^Changes to be committed\:$/;
+my $unstaged_regex = qr/^Changes not staged for commit\:$/;
+my $untracked_regex = qr/^Untracked files\:$/;
+my $empty_line = qr/^\n$/;
 
-my %filetypes_to_arrays = (
-    # "UNSET" => []
-    "NEW" => []
-    , "MODIFIED" => []
-    , "DELETED" => []
-    , "UNTRACKED" => []
-);
-
-my %filetype_labels = (
-    "NEW" => "new files:"
-    , "MODIFIED" => "modified:"
-    , "DELETED" => "deleted:"
-    , "UNTRACKED" => "untracked:"
-);
-
-my %filetype_color_codes = (
-    "NEW" => $staged
-    , "MODIFIED" => $unstaged
-    , "DELETED" => $unstaged
-    , "UNTRACKED" => $unstaged
-);
-
-my $current_filetype = "UNSET";
+my @filenames = ();
+my $status;
+my $empty_line_count = 0;
 
 sub main {
   while (<>) {
-    if($_ =~ $new_regex) {
-      if($current_filetype ne "NEW" && $filetypes_to_arrays{$current_filetype}) {
-        # print "$current_filetype";
-        if($filetype_labels{$current_filetype}) {
-          print $default."\t".$filetype_labels{$current_filetype}
-        }
+    if($_ =~ $filename_regex) {
+      push @filenames, $1;
 
-        print array_to_string($filetype_color_codes{$current_filetype}, @{$filetypes_to_arrays{$current_filetype}});
+    } elsif($_ =~ $staged_regex) {
+      print_empty_lines();
+      print $default."$_";
 
+    } elsif($_ =~ $unstaged_regex) {
+      if(@filenames) {
+        print "\t".array_to_string($staged, @filenames);
+        @filenames = ();
       }
-      push @{$filetypes_to_arrays{"NEW"}}, $1;
-      $current_filetype = "NEW";
-
-    } elsif($_ =~ $modified_regex) {
-      if($current_filetype ne "MODIFIED" && $filetypes_to_arrays{$current_filetype}) {
-        if($filetype_labels{$current_filetype}) {
-          print $default."\t".$filetype_labels{$current_filetype}
-        }
-
-        print array_to_string($filetype_color_codes{$current_filetype}, @{$filetypes_to_arrays{$current_filetype}});
-      }
-      push @{$filetypes_to_arrays{"MODIFIED"}}, $1;
-      $current_filetype = "MODIFIED";
-
-    } elsif($_ =~ $deleted_regex) {
-      if($current_filetype ne "DELETED" && $filetypes_to_arrays{$current_filetype}) {
-        if($filetype_labels{$current_filetype}) {
-          print $default."\t".$filetype_labels{$current_filetype}
-        }
-
-        print array_to_string($filetype_color_codes{$current_filetype}, @{$filetypes_to_arrays{$current_filetype}});
-      }
-      push @{$filetypes_to_arrays{"DELETED"}}, $1;
-      $current_filetype = "DELETED";
+      print_empty_lines();
+      print $default."$_";
 
     } elsif($_ =~ $untracked_regex) {
-       if($current_filetype ne "UNTRACKED" && $filetypes_to_arrays{$current_filetype}) {
-        if($filetype_labels{$current_filetype}) {
-          print $default."\t".$filetype_labels{$current_filetype}
-        }
-
-        print array_to_string($filetype_color_codes{$current_filetype}, @{$filetypes_to_arrays{$current_filetype}});
+      if(@filenames) {
+        print "\t".array_to_string($unstaged, @filenames);
+        @filenames = ();
       }
-      push @{$filetypes_to_arrays{"UNTRACKED"}}, $1;
-      $current_filetype = "UNTRACKED";
+      print_empty_lines();
+      print $default."$_";
+
+    } elsif($_ =~ $empty_line) {
+      $empty_line_count++;
 
     } else {
-       if($current_filetype ne "UNSET" && $filetypes_to_arrays{$current_filetype}) {
-        if($filetype_labels{$current_filetype}) {
-          print $default."\t".$filetype_labels{$current_filetype}
-        }
-        print array_to_string($filetype_color_codes{$current_filetype}, @{$filetypes_to_arrays{$current_filetype}});
-      }
-      $current_filetype = "UNSET";
-
+      print_empty_lines();
       print $default."$_";
     }
   }
+
+  if(@filenames) {
+    print "\t".array_to_string($unstaged, @filenames);
+    @filenames = ();
+  }
+  print_empty_lines();
 }
 
 sub array_to_string {
     my ($color, @array) = @_;
-    my $text = $color."\t";
+    # my $text = $color."\t";
+    my $text = "$color";
 
     foreach my $element (@array) {
       $text .= escape_spaces($element)." ";
@@ -114,6 +76,13 @@ sub escape_spaces {
     my ($input_string) = @_;
     $input_string =~ s/ /\\ /g;
     return $input_string;
+}
+
+sub print_empty_lines {
+ for my $count (1..$empty_line_count) {
+    print "\n";
+  }
+  $empty_line_count = 0;
 }
 
 main();
